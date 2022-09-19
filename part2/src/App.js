274ec9ce2027769e1849accdaad9760ad3cdc5e4
mvdn28 <vieira.mauricio94@gmail.com>
2022-09-19 16:34:25 -0300

    modified:   part2/src/App.js new file:   part2/src/App_2-1to2-5.js new file:   part2/src/App_Notes.js

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,61 +1,105 @@
 import React from 'react';
-//import { useState } from 'react'
-import Course from './components/Course'
+import { useState } from 'react'
 
+const Filter=(props)=>{
+
+  return(
+    <p>Filter shown with: <input onChange={props.handleChange} /></p>
+  )
+}
+
+const PersonForm = (props)=>{
+  let addNewPerson = props.handleSubmit
+  let newPerson=props.newPerson
+  let handlePersonChange=props.handleInputChange
+
+  return(
+    <form onSubmit={addNewPerson}>
+        <div>
+          <p>name: <input value={newPerson.name} onChange={handlePersonChange} name='name' /></p>
+          <p>number: <input value={newPerson.number} onChange={handlePersonChange} name='number' /></p>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+  )
+}
+
+const Persons= (props) =>{
+  let filteredList = props.list
+  return(
+    <ul>
+      {filteredList.map((person,i)=>
+        <li key={i}>{person.name} {person.number}</li>
+      )}
+    </ul>
+  )
+}
 
 const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas',
+    number:'040-1234567'}
+  ]) 
+  const [newPerson, setNewPerson] = useState([{name:'',number:''}])
+  const [filteredList,setFilteredList]=useState(persons)
+  
+  const handleListChange=(event)=>{
+    const query = event.target.value;
+    let updatedList = [...persons];
+    updatedList=updatedList.filter((person) => person.name.indexOf(query) !== -1)
+    setFilteredList(updatedList)
+  }
+  
+  const handlePersonChange = (event) => {
+    const { name, value } = event.target;
+    setNewPerson({
+      ...newPerson,
+      [name]: value,
+    });
+  }
+
+  function addNewPerson(event) {
+    event.preventDefault();
+    const personObject = {
+      name: newPerson.name,
+      number: newPerson.number
+    };
+
+    let checkName = persons.some(e => e.name === newPerson.name);
+    let checkNumber = persons.some(e => e.number === newPerson.number);
+
+    if (checkNumber || checkName) {
+      alert(newPerson.name + ' is already added to the phonebook');
+      setNewPerson([{ name: '', number: '' }]);
+      let updatedList = [...persons];
+      setFilteredList(updatedList);
+      event.target.reset();
+    } else {
+      setPersons(persons.concat(personObject));
+      setNewPerson([{ name: '', number: '' }]);
+      let updatedList = [...persons];
+      setFilteredList(updatedList);
+      event.target.reset();
     }
-  ]
+  }
 
   return (
-    <><h1>Web development curriculum</h1>
-    {courses.map(course=>
-          <Course key={course.id} course={course}/>
-          )}
+    <><div>
+      <h2>Phonebook</h2>
+      <Filter handleChange={handleListChange}/>
+      <h3>Add a new</h3>
+      <PersonForm
+        handleSubmit={addNewPerson}
+        newPerson={newPerson}
+        handleInputChange={handlePersonChange}
+      />
+      <h2>Numbers</h2>
+      <Persons list={filteredList} />
+    </div>
     </>
-    )
+  )
 }
 
 export default App
