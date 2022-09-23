b3aa2320f268c277ec1f29e0076d70f5aa6d6239
mvdn28 <vieira.mauricio94@gmail.com>
2022-09-23 10:57:09 -0300

    modified:   part2/.gitignore new file:   part2/db.JSON new file:   part2/db_note.JSON modified:   part2/package-lock.json modified:   part2/package.json modified:   part2/src/App.js new file:   part2/src/App_2-6to2-11.js renamed:    part2/src/App_Notes.js -> part2/src/App_note.js modified:   part2/src/index.js

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,105 +1,133 @@
 import React from 'react';
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
+import axios from 'axios'
 
-const Filter=(props)=>{
-
-  return(
-    <p>Filter shown with: <input onChange={props.handleChange} /></p>
-  )
+const CountryShow= (props)=>{
+  let country = props.country
+  let show = props.show
+  let capital = country.capital
+  let lat = country.capitalInfo.latlng[0]
+  let lon = country.capitalInfo.latlng[1]
+  console.log(lat,lon)
+  let api = props.api
+  const hook = () => {
+    axios
+      .get('https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&appid='+api+'&units=metric')
+      .then(response => {
+        capital['temp']=response.data.main.temp
+        capital['wind']=response.data.wind.speed
+        capital['icon']="http://openweathermap.org/img/w/"+response.data.weather[0].icon+".png"
+      })
+  }
+  
+  useEffect(hook, [])
+  console.log(capital)
+    if(show){
+    return(
+      <><h1>{country.name.common}</h1>
+        <p>capital: {country.capital[0]}</p>
+        <p>area: {country.area}</p>
+        <h2>Languages:</h2>
+        <ul>
+          {Object.keys(country.languages).map(key =>{
+            return(
+              <li key={key}>{country.languages[key]}</li>
+            )
+          })}
+        </ul>
+        <img src={country.flags.png} alt={country.name}/>
+        <h1>Weather in {country.capital[0]}</h1>
+        <p>temperature: {capital.temp} Ceslsius </p>
+        <div><img src={capital.icon} id="icon"/></div>
+        <p>Wind: {capital.wind} m/s </p>
+        </>
+    )
+    }else{
+      return(
+        <>{country.name.common} {country.show}</>
+      )
+    }
 }
 
-const PersonForm = (props)=>{
-  let addNewPerson = props.handleSubmit
-  let newPerson=props.newPerson
-  let handlePersonChange=props.handleInputChange
-
-  return(
-    <form onSubmit={addNewPerson}>
-        <div>
-          <p>name: <input value={newPerson.name} onChange={handlePersonChange} name='name' /></p>
-          <p>number: <input value={newPerson.number} onChange={handlePersonChange} name='number' /></p>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-  )
-}
-
-const Persons= (props) =>{
-  let filteredList = props.list
-  return(
-    <ul>
-      {filteredList.map((person,i)=>
-        <li key={i}>{person.name} {person.number}</li>
-      )}
-    </ul>
-  )
-}
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-    number:'040-1234567'}
-  ]) 
-  const [newPerson, setNewPerson] = useState([{name:'',number:''}])
-  const [filteredList,setFilteredList]=useState(persons)
+
+  const [countries,setCountries]=useState([])
+  const [filteredList,setFilteredList]=useState(countries)
+  const api_key = process.env.REACT_APP_API_KEY
+  console.log(api_key)
+
+  const hook = () => {
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then(response => {
+        setCountries(response['data'])
+      })
+  }
   
-  const handleListChange=(event)=>{
+  useEffect(hook, [])
+
+  const handleCountryChange=(event)=>{
     const query = event.target.value;
-    let updatedList = [...persons];
-    updatedList=updatedList.filter((person) => person.name.indexOf(query) !== -1)
-    setFilteredList(updatedList)
-  }
-  
-  const handlePersonChange = (event) => {
-    const { name, value } = event.target;
-    setNewPerson({
-      ...newPerson,
-      [name]: value,
-    });
-  }
-
-  function addNewPerson(event) {
-    event.preventDefault();
-    const personObject = {
-      name: newPerson.name,
-      number: newPerson.number
-    };
-
-    let checkName = persons.some(e => e.name === newPerson.name);
-    let checkNumber = persons.some(e => e.number === newPerson.number);
-
-    if (checkNumber || checkName) {
-      alert(newPerson.name + ' is already added to the phonebook');
-      setNewPerson([{ name: '', number: '' }]);
-      let updatedList = [...persons];
-      setFilteredList(updatedList);
-      event.target.reset();
-    } else {
-      setPersons(persons.concat(personObject));
-      setNewPerson([{ name: '', number: '' }]);
-      let updatedList = [...persons];
-      setFilteredList(updatedList);
-      event.target.reset();
+    if(query.length>0){
+      let updatedList = [...countries];
+      updatedList=updatedList.filter((country) => country.name.common.indexOf(query) !== -1)
+      updatedList.map(country=>{
+        return(
+          country.show=false
+        )
+      })
+      setFilteredList(updatedList)
+    }else{
+      setFilteredList(countries)
     }
+    
   }
 
-  return (
-    <><div>
-      <h2>Phonebook</h2>
-      <Filter handleChange={handleListChange}/>
-      <h3>Add a new</h3>
-      <PersonForm
-        handleSubmit={addNewPerson}
-        newPerson={newPerson}
-        handleInputChange={handlePersonChange}
-      />
-      <h2>Numbers</h2>
-      <Persons list={filteredList} />
-    </div>
-    </>
-  )
+  function handleCountryShow(List,element){
+    const copy=[
+      ...List
+    ]
+    copy[element].show=!copy[element].show
+    setFilteredList(copy)
+    console.log(copy)
+  }
+  if (filteredList['data']!==undefined){
+    filteredList=filteredList['data']
+  }
+
+
+
+  if (filteredList.length===1){
+    let country=filteredList[0]
+    country.show=true
+    return(
+      <><p>
+        Find countries:
+        <input type="text" onChange={handleCountryChange}></input>
+      </p><CountryShow country={country} show={country.show} api={api_key} /></>
+    )
+  }else if(filteredList.length>1 && filteredList.length<=10){
+    return(
+      <><p>
+        Find countries: 
+        <input type="text" onChange={handleCountryChange}></input>
+      </p>
+      <ul>
+        {filteredList.map((country, i) => 
+        <><li key={i}>
+        <button onClick={() => handleCountryShow(filteredList,i)}>{country.show ?'Hide':'Show'} {country.name.common}</button>
+        <CountryShow country={country} show={country.show} api={api_key} /></li></>)}
+      </ul></>
+    )
+  }else{
+    return(
+      <><p>
+        Find countries:
+        <input type="text" onChange={handleCountryChange}></input>
+      </p><p>Too many matches, specify another filter</p></>
+    )
+  }
 }
 
 export default App
