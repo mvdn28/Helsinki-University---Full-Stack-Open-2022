1209472e3487605f2d4e4ae81ca4c27bf747fd2e
mvdn28 <vieira.mauricio94@gmail.com>
2022-10-05 09:34:22 -0300

    modified:   part3/index.js modified:   part3/test.rest modified:   part3Examples/index.js modified:   part3Examples/test.rest

--- part3/index.js
+++ part3/index.js
@@ -4,12 +4,21 @@ const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
 
+const requestLogger = (request, response, next) =>{
+  console.log('Method:',request.method)
+  console.log('Path:',request.path)
+  console.log('Body:',request.body)
+  console.log('---')
+  next()
+}
+
 
 app.use(cors())
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] :response-time ms :body'))
 app.use(express.static('build'))
+app.use(requestLogger)
 
 const Person = require('./models/person')
 
@@ -37,10 +46,9 @@ let persons = [
 ]
 // -----------Functions ---------------------------
 const entriesNow = () => {
-    const entries = persons.length > 0
-        ? Math.max(...persons.map(n => n.id))
-        : 0
-    return entries
+  Person.countDocuments({}, function( err, count){
+    return count
+  })
 }
 
 //---------------GET--------------------------------
@@ -51,26 +59,35 @@ app.get('/api/persons', (request,response)=>{
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
+app.get('/api/persons/:id', (request, response,next) => {
+  Person.findById(request.params.id)
+  .then(person => {
+    if(person){
+      response.json(person)
+    }else{
+      response.status(404).end()
+    }
   })
-
+  .catch(error=> next(error))
 })
 
 app.get('/info', (request, response) => {
+  Person.countDocuments({},function(err, count){
+    console.log("Number of docs:",count)
     response.send(
-        `<p>Phonebook has info for ${entriesNow()} people</p>
-        <p> ${new Date()} </p>`)
+      `<p>Phonebook has info for ${count} people</p>
+      <p> ${new Date()} </p>`)
+  })  
 })
 
 //------------------------------DELETE---------------------------
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-  
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response,next) => {
+    Person.findByIdAndDelete(request.params.id)
+      .then(result=>{
+        response.status(204).end()
+      })
+      .catch(error=>next(error))
   })
 
 //--------------------------POST ---------------------------
@@ -95,7 +112,38 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
+//---------------PUT----------------------------
+app.put('/api/persons/:id', (request,response, next)=>{
+  const body = request.body
+  
+  const person={
+    name: body.name,
+    number:body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, {new: true})
+    .then(updatedPerson=>{
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
 //--------------Middleware ----------------------
+
+const unknownEndpoint = (request, response)=>{
+  response.status(404).send({error:'unknown endpoint'})
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler=(error,request,response,next) =>{
+  console.log(error.message)
+  if(error.name === 'CastError'){
+    return response.status(400).send({error:'malformatted id'})
+  }
+
+  next(error)
+}
 
 
 
--- part3Examples/index.js
+++ part3Examples/index.js
@@ -1,10 +1,17 @@
 require('dotenv').config()
 
-const http = require('http')
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
+
+const requestLogger = (request, response, next) =>{
+  console.log('Method:',request.method)
+  console.log('Path:',request.path)
+  console.log('Body:',request.body)
+  console.log('---')
+  next()
+}
 
 
 app.use(cors())
@@ -12,8 +19,10 @@ morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(express.json())
 app.use(express.static('build'))
 app.use(morgan(':method :url :status :res[content-length] :response-time ms :body'))
+app.use(requestLogger)
 
 const Note = require('./models/note')
+const { nextTick } = require('process')
 
 let notes = [
   {
@@ -44,18 +53,42 @@ app.get('/api/notes', (request, response) => {
       })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
+app.get('/api/notes/:id', (request, response,next) => {
+  Note.findById(request.params.id)
+  .then(note => {
+    if(note){
+      response.json(note)
+    }else{
+      response.status(404).end()
+    }
+  })
+  .catch(error =>{
+    next(error)
   })
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
+app.delete('/api/notes/:id', (request, response,next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(result=>{
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/notes/:id', (request,response, next)=>{
+  const body = request.body
   
-    response.status(204).end()
-  })
+  const note={
+    content:body.content,
+    important:body.important
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, {new: true})
+    .then(updatedNote=>{
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
 
 app.post('/api/notes', (request, response) => {
     const body = request.body
@@ -77,6 +110,21 @@ app.post('/api/notes', (request, response) => {
         response.json(savedNote)
       })
 })
+
+const unknownEndpoint = (request, response)=>{
+  response.status(404).send({error:'unknown endpoint'})
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler=(error,request,response,next) =>{
+  console.log(error.message)
+  if(error.name === 'CastError'){
+    return response.status(400).send({error:'malformatted id'})
+  }
+
+  next(error)
+}
 
 
 const PORT = process.env.PORT
