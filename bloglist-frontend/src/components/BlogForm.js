5f9e83cebfbcb777cf3e94e720ba94cdf3529b69
mvdn28 <vieira.mauricio94@gmail.com>
2022-11-16 14:50:40 -0300

    mudanca de pc

---
+++ bloglist-frontend/src/components/BlogForm.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+const BlogForm = ({createBlog}) => {
+    const [title, setTitle] = useState('')
+    const [author, setAuthor] = useState('')
+    const [url, setUrl] = useState('')
+
+    const handleTitleChange = (event) => {
+        setTitle(event.target.value)
+    }
+    const handleAuthorChange = (event) => {
+        setAuthor(event.target.value)
+    }
+    const handleUrlChange = (event) => {
+        setUrl(event.target.value)
+    }
+
+    const addBlog = (event) => {
+        event.preventDefault()
+        createBlog({
+            title,author,url
+        })
+
+        setTitle('')
+        setAuthor('')
+        setUrl('')
+    }
+
+    return(
+        <form onSubmit={addBlog}>
+            <div>
+                title: 
+                <input type="text" value={title} name="Title" onChange={handleTitleChange}/>
+            </div>
+            <div>
+                author: 
+                <input type="text" value={author} name="Author" onChange={handleAuthorChange}/>
+            </div>
+            <div>
+                url: 
+                <input type="text" value={url} name="Url" onChange={handleUrlChange}/>
+            </div>
+            <button type='submit'>create</button>
+        </form>
+    )
+
+
+}
+
+export default BlogForm
