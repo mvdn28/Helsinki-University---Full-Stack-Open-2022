44729dd39c6f1b2fb3bf73a6453c0989123c8ded
mvdn28 <vieira.mauricio94@gmail.com>
2022-12-20 16:00:51 -0300

    finished exercises 6.1-6.2

---
+++ unicafe-redux/src/reducer.js
@@ -0,0 +1,38 @@
+const initialState = {
+  good: 0,
+  ok: 0,
+  bad: 0
+}
+
+const counterReducer = (state = initialState, action) => {
+  console.log(action)
+  switch (action.type) {
+    case 'GOOD':
+      const stateToChangeGood = state.good
+      const changedStateGood = {
+        ...state,
+        good: stateToChangeGood+1
+      }
+      return changedStateGood
+    case 'OK':
+      const stateToChangeOk = state.ok
+      const changedStateOk = {
+        ...state,
+        ok: stateToChangeOk+1
+      }
+      return changedStateOk
+    case 'BAD':
+      const stateToChangeBad = state.bad
+      const changedStateBad = {
+        ...state,
+        bad: stateToChangeBad+1
+      }
+      return changedStateBad
+    case 'ZERO':
+      return initialState
+    default: return state
+  }
+  
+}
+
+export default counterReducer
