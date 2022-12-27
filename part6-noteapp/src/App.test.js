6dee23d98fcf4874bf3756a258608163f07a370b
mvdn28 <vieira.mauricio94@gmail.com>
2022-12-27 11:27:24 -0300

    modified:   part6-noteapp/package-lock.json modified:   part6-noteapp/package.json modified:   part6-noteapp/src/App.js modified:   part6-noteapp/src/App.test.js modified:   part6-noteapp/src/components/Notes.js new file:   part6-noteapp/src/components/VisibilityFilter.js modified:   part6-noteapp/src/index.js new file:   part6-noteapp/src/reducers/filterReducer.js modified:   part6-noteapp/src/reducers/noteReducer.js

--- part6-noteapp/src/App.test.js
+++ part6-noteapp/src/App.test.js
@@ -1,8 +1,50 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import noteReducer from './reducers/noteReducer'
+import deepFreeze from 'deep-freeze'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('noteReducer', () => {
+  test('returns new state with action notes/createNote', () => {
+    const state = []
+    const action = {
+      type: 'notes/createNote',
+      payload: 'the app state is in redux store',
+    }
+
+    deepFreeze(state)
+    const newState = noteReducer(state, action)
+
+    expect(newState).toHaveLength(1)
+    expect(newState.map(s => s.content)).toContainEqual(action.payload)
+  })
+
+  test('returns new state with action notes/toggleImportanceOf', () => {
+    const state = [
+      {
+        content: 'the app state is in redux store',
+        important: true,
+        id: 1
+      },
+      {
+        content: 'state changes are made with actions',
+        important: false,
+        id: 2
+      }]
+  
+    const action = {
+      type: 'notes/toggleImportanceOf',
+      payload: 2
+    }
+  
+    deepFreeze(state)
+    const newState = noteReducer(state, action)
+  
+    expect(newState).toHaveLength(2)
+  
+    expect(newState).toContainEqual(state[0])
+  
+    expect(newState).toContainEqual({
+      content: 'state changes are made with actions',
+      important: true,
+      id: 2
+    })
+  })
+})
