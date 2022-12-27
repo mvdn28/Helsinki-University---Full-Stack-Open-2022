f8cd43153eb371cc9189f665a70966bfd3164d7f
mvdn28 <vieira.mauricio94@gmail.com>
2022-12-27 09:28:18 -0300

    6.a end

--- part6-noteapp/src/App.js
+++ part6-noteapp/src/App.js
@@ -1,58 +1,13 @@
-import './App.css';
-import { createStore } from 'redux'
-
-const noteReducer = (state = [], action) => {
-  switch(action.type) {
-    case 'NEW_NOTE':
-      return state.concat(action.data)
-    case 'TOGGLE_IMPORTANCE': {
-      const id = action.data.id
-      const noteToChange = state.find(n => n.id === id)
-      const changedNote = { 
-        ...noteToChange, 
-        important: !noteToChange.important 
-      }
-      return state.map(note =>
-        note.id !== id ? note : changedNote 
-      )
-     }
-    default:
-      return state
-  }
-}
-
-const store = createStore(noteReducer)
-
-store.dispatch({
-  type: 'NEW_NOTE',
-  data: {
-    content: 'the app state is in redux store',
-    important: true,
-    id: 1
-  }
-})
-
-store.dispatch({
-  type: 'NEW_NOTE',
-  data: {
-    content: 'state changes are made with actions',
-    important: false,
-    id: 2
-  }
-})
+import Notes from './components/Notes'
+import NewNote from './components/NewNote'
 
 const App = () => {
-  return(
-    <div>
-      <ul>
-        {store.getState().map(note=>
-          <li key={note.id}>
-            {note.content} <strong>{note.important ? 'important' : ''}</strong>
-          </li>
-        )}
-        </ul>
-    </div>
-  )
+  return (
+      <div>
+        <NewNote />
+        <Notes />
+      </div>
+    )
 }
 
-export default App;
+export default App
